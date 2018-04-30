aaa2a7e99e541d4b6157a565c72c1dfc040e9698
PaulinePro <paulinedelhaye6298@gmail.com>
2018-04-30 13:08:49 +0200

    parc ok

--- parc.php
+++ parc.php
@@ -10,41 +10,43 @@
   <body>
     <?php include 'php/navbar.php'; ?>
 
-    <h1 class="text-center">Parc</h1><br />
+    <h1 class="text-center">Parc</h1><br/>
+    <div class="container">
     <div class="table-responsive">
         <table class="table">
             <thead>
                 <tr>
-                    <th>Nom</th>
+                    <th>Nom composant</th>
                     <th>N° de série</th>
-                    <th>Type</th>
+                    <th>Adresse Mac</th>
                     <th>Modèle</th>
                     <th>Fabricant</th>
-                    <th>Adresse Mac</th>
+                    <th>Type</th>
                     <th>Salle</th>
                 </tr>
             </thead>
             <tbody>
                 <tr>
-                    <td>Cell 1</td>
-                    <td>Cell 2</td>
-                    <td>Cell 3</td>
-                    <td>Cell 4</td>
-                    <td>Cell 5</td>
-                    <td>Cell 6</td>
-                    <td>Cell 7</td>
-                </tr>
-                <tr>
-                    <td>Cell 3</td>
-                    <td>Cell 4</td>
-                    <td>Cell 3</td>
-                    <td>Cell 4</td>
-                    <td>Cell 5</td>
-                    <td>Cell 6</td>
-                    <td>Cell 7</td>
+                  <?php
+                  $reqcomposant = $bdd->prepare("SELECT nomComposant, numSerie, adresseMac, modeleComposant, fabricantComposant, typeComposant, COMPOSANT.idSalle, libelleSalle FROM `COMPOSANT`, `SALLE` WHERE COMPOSANT.idSalle = SALLE.idSalle");
+                  $reqcomposant->execute();
+                  $dbrep = $reqcomposant->fetchAll();
+                  foreach ($dbrep as $row){
+                    echo "<tr>";
+                    echo "<td>".$row['nomComposant']."</td>"; //Affiche dans la colonne les infos de la bdd
+                    echo "<td>".$row['numSerie']."</td>";
+                    echo "<td>".$row['adresseMac']."</td>";
+                    echo "<td>".$row['modeleComposant']."</td>";
+                    echo "<td>".$row['fabricantComposant']."</td>";
+                    echo "<td>".$row['typeComposant']."</td>";
+                    echo "<td>".$row['idSalle']."</td>";
+                    echo "</tr>";
+                  }
+                  ?>
                 </tr>
             </tbody>
         </table>
+      </div>
     </div>
 
     <?php include 'php/footer.php'; ?>
